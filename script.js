388fce38fdb37b5082b8a541a65eaaba3f2cc8bf
Nikhil Prajapat <nikhilprajapat59761@gmail.com>
2025-06-30 11:40:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document
   .getElementById("openPdfLink")
   .addEventListener("click", function (event) {
     event.preventDefault(); // Prevent default anchor behavior
-    const pdfPath = "/images/MY RESUME.pdf"; // Path to your PDF file
+    const pdfPath = "images/MY RESUME.pdf"; // Path to your PDF file
     window.open(pdfPath, "_blank"); // Open the PDF in a new tab
   });
